16f51fe8ae827baaa74165f509946b85bc0e4387
lecture <Robbie>
2023-02-20 00:31:39 +0900

    init commit

---
+++ src/main/java/com/example/springiocdilivelecture230220/SpringIocDiLiveLecture230220Application.java
@@ -0,0 +1,13 @@
+package com.example.springiocdilivelecture230220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocDiLiveLecture230220Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIocDiLiveLecture230220Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springiocdilivelecture230220/SpringIocDiLiveLecture230220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springiocdilivelecture230220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocDiLiveLecture230220ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
